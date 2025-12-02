0e74b0ed253f859370e87ace9e8c772d9d111486
Filipe <filipe.candido.rodrigues@gmail.com>
2025-12-02 15:49:11 -0300

    feat: cria classe ClienteService

--- src/main/java/io/github/filipecrodrigues/service/ClienteService.java
+++ src/main/java/io/github/filipecrodrigues/service/ClienteService.java
@@ -1,4 +1,52 @@
 package io.github.filipecrodrigues.service;
 
+
+import io.github.filipecrodrigues.entity.Cliente;
+import io.github.filipecrodrigues.repository.ClienteRepository;
+import org.springframework.stereotype.Service;
+
+
+import java.util.List;
+
+
+@Service
 public class ClienteService {
+
+
+    private final ClienteRepository repository;
+
+
+    public ClienteService(ClienteRepository repository) {
+        this.repository = repository;
+    }
+
+
+    public Cliente salvar(Cliente cliente) {
+        return repository.save(cliente);
+    }
+
+
+    public List<Cliente> listarTodos() {
+        return repository.findAll();
+    }
+
+
+    public Cliente buscarPorId(Long id) {
+        return repository.findById(id).orElseThrow();
+    }
+
+
+    public Cliente atualizar(Long id, Cliente novoCliente) {
+        Cliente cliente = buscarPorId(id);
+        cliente.setNome(novoCliente.getNome());
+        cliente.setEmail(novoCliente.getEmail());
+        cliente.setCpf(novoCliente.getCpf());
+        cliente.setTelefone(novoCliente.getTelefone());
+        return repository.save(cliente);
+    }
+
+
+    public void deletar(Long id) {
+        repository.deleteById(id);
+    }
 }
