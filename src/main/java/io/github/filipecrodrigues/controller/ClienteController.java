5c18585bbf9f833f54ccdee8c1a511929d96faef
Filipe <filipe.candido.rodrigues@gmail.com>
2025-12-02 15:41:40 -0300

    feat: cria classe ClienteController

--- src/main/java/io/github/filipecrodrigues/controller/ClienteController.java
+++ src/main/java/io/github/filipecrodrigues/controller/ClienteController.java
@@ -1,4 +1,57 @@
 package io.github.filipecrodrigues.controller;
 
+
+
+import io.github.filipecrodrigues.entity.Cliente;
+import io.github.filipecrodrigues.service.ClienteService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/clientes")
 public class ClienteController {
+
+
+    private final ClienteService service;
+
+
+    public ClienteController(ClienteService service) {
+        this.service = service;
+    }
+
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente criar(@RequestBody Cliente cliente) {
+        return service.salvar(cliente);
+    }
+
+
+    @GetMapping
+    public List<Cliente> listar() {
+        return service.listarTodos();
+    }
+
+
+    @GetMapping("/{id}")
+    public Cliente buscar(@PathVariable Long id) {
+        return service.buscarPorId(id);
+    }
+
+
+    @PutMapping("/{id}")
+    public Cliente atualizar(@PathVariable Long id, @RequestBody Cliente cliente) {
+        return service.atualizar(id, cliente);
+    }
+
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletar(@PathVariable Long id) {
+        service.deletar(id);
+    }
 }
